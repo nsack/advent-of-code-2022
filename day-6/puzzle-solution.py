5cda5ad3739d5ea37b0f4854a821cdc454e2e4a1
nsack <nicholas.sack@hey.com>
2022-12-06 09:22:42 -0500

    feature: day 6 solution

---
+++ day-6/puzzle-solution.py
@@ -0,0 +1,20 @@
+def find_marker(input_str: str, marker_length: int):
+    """
+    """
+    for i in range(len(input_str)):
+            if marker_length == len(set(input_str[i:i+marker_length])):
+                break
+
+    return i + marker_length
+
+def puzzle_solution():
+    marker_length = 4
+    marker_length_part_2 = 14
+    with open("puzzle-input.txt", "r") as f:
+        # read the lines
+        input_str = f.read()
+    print(find_marker(input_str, 4))
+    print(find_marker(input_str, 14))
+
+
+puzzle_solution()
